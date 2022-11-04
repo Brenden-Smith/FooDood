bfaf54af3b56bb6a1aecd79fa2777b2ae7d7001f
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2022-11-04 15:17:47 -0700

    Hello world backend

---
+++ api/src/main/java/dev/foodood/api/controllers/DefaultController.java
@@ -0,0 +1,15 @@
+package dev.foodood.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/")
+public class DefaultController {
+
+    @GetMapping(path="/", produces="text/plain")
+    public String index() {
+        return "Hello World!";
+    }
+}
