2e65dd7b8d90383f9d95ace0105fbb3e2976b4ba
Brenden Smith <git@brenden-smith.com>
2023-02-10 02:25:22 -0800

    Auth flow

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler';
-import Navigator from './navigation/Navigator';
+import { ActivityIndicator, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
+import Navigator from "./navigation/Navigator";
+import "./service/firebase";
+import Login from "./screens/auth/Login";
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Navigator />
-    </NavigationContainer>
+  const [signedIn, setSignedIn] = useState<boolean | undefined>();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(getAuth(), (user) => {
+      setMounted(true);
+      if (user) {
+        setSignedIn(true);
+      } else {
+        setSignedIn(false);
+      }
+    });
+  });
+
+  return mounted && signedIn !== undefined ? (
+    signedIn ? (
+      <NavigationContainer>
+        <Navigator />
+      </NavigationContainer>
+    ) : (
+      <Login />
+    )
+  ) : (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ActivityIndicator />
+    </View>
   );
 }
