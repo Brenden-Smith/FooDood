847c0975e0caf88c3a605efed750496af193fefa
Rodolfo Sandoval <rodolfo.rudy.sandoval@gmail.com>
2023-02-10 04:22:28 -0800

    added tag's screen added icons to navbar

--- client/navigation/Navigator.tsx
+++ client/navigation/Navigator.tsx
@@ -1,14 +1,21 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Tags, Plates, Likes } from '../screens';
+import { Image } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export default function NavTab() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Plates" component={Plates} />
-      <Tab.Screen name="Tags" component={Tags} />
-      <Tab.Screen name="Likes" component={Likes} />
+    <Tab.Navigator
+      screenOptions= {{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tab.Screen name="Tags" component={Tags} options={{tabBarIcon:() =><Image style={{width: 25, height: 25,}} source={require("../assets/icons/tag-solid.png")}/>}}/>
+      <Tab.Screen name="Plates" component={Plates} options={{tabBarIcon:() =><Image style={{width: 25, height: 25,}} source={require("../assets/icons/plate-utensils.png")}/>}}/>
+      <Tab.Screen name="Likes" component={Likes} options={{tabBarIcon:() =><Image style={{width: 25, height: 25,}} source={require("../assets/icons/star-solid.png")}/>}}/>
     </Tab.Navigator>
   );
 }
--- client/screens/home/Tags.tsx
+++ client/screens/home/Tags.tsx
@@ -1,7 +1,32 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 export default function Tags() {
     return (
-        <Text>Test Pages for Tags</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="mb-5 text-3xl font-bold">Select Tags</Text>
+            <View className="flex-1 items-center rounded w-4/5 py-5 bg-slate-200 my-5">
+                <Text className="text-2xl p-2">Cuisine</Text>
+                <View className="grid">
+                    <View className="w-16"><Button title="Tag"/></View>
+                    <View className="w-16"><Button title="Tag"/></View>
+                    <View className="w-16"><Button title="Tag"/></View>
+                </View>
+            </View>
+            <View className="flex-1 items-center rounded w-4/5 py-5 bg-slate-200 my-5">
+                <Text className="text-2xl p-2">Styles</Text>
+                <View className="grid">
+                    <View className="w-16"><Button title="Tag"/></View>
+                    <View className="w-16"><Button title="Tag"/></View>
+                    <View className="w-16"><Button title="Tag"/></View>
+                </View>
+            </View>
+            <View className="flex-1 items-center rounded w-4/5 py-5 bg-slate-200 my-5">
+                <Text className="text-2xl p-2">Extra</Text>
+                <View className="grid">
+                    <View className="w-16"><Button title="Tag"/></View>
+                    <View className="w-16"><Button title="Tag"/></View>
+                </View>
+            </View>
+        </View>
     );
 }
