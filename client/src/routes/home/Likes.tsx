57bc13eeb33817c1781e584a3e15209c331ad851
konradmh3 <konradm.h.3@gmail.com>
2023-02-23 13:08:30 -0800

    Fixed back button to exit settings, was giving error 'setSettingsOpen(false) not a function'

--- client/src/routes/home/Likes.tsx
+++ client/src/routes/home/Likes.tsx
@@ -8,91 +8,97 @@ import {
 	Modal,
 	TextInput,
 	Switch,
-} from "react-native";
-
-import React, { useState, useEffect } from "react";
-import {
+  } from "react-native";
+  
+  import React, { useState, useEffect } from "react";
+  import {
 	getDocs,
 	query,
 	collection,
 	getFirestore,
 	where,
 	DocumentData,
-} from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { ScrollView } from "react-native-gesture-handler";
-
-function Settings(setSettingsOpen: any) {
+  } from "firebase/firestore";
+  import { getAuth } from "firebase/auth";
+  import { ScrollView } from "react-native-gesture-handler";
+  
+  interface SettingsProps {
+	setSettingsOpen: (value: boolean) => void;
+  }
+  
+  function Settings(props: SettingsProps) {
+	const { setSettingsOpen } = props;
 	return (
-		<View className="flex-1 items-center justify-center bg-white">
-			{/* create a function which calls the setSettingsOpen function that closes the settings dialogue  */}
-			<TouchableOpacity
-				onPress={() => {
-					setSettingsOpen(false);
-				}}
-			>
-				<Image source={require("../../assets/icons/back.png")} />
-			</TouchableOpacity>
-
-			<Text className="mb-5 text-3xl font-bold">Settings</Text>
-			<View className="flex-1 items-center rounded w-4/5 py-5 bg-slate-200 my-5">
-				<Text className="text-2xl p-2">Account</Text>
-				<View className="grid">
-					<View className="w-64 flex flex-row">
-						{/* create a textfield which loads in the current email */}
-						<TextInput
-							placeholder="Email"
-							className="rounded bg-white w-32"
-						/>
-
-						<TouchableOpacity className="rounded bg-cyan-600">
-							<Text>Submit</Text>
-						</TouchableOpacity>
-					</View>
-					<View className="w-64 flex flex-row">
-						{/* create a textfield which loads in the current username */}
-						<TextInput
-							placeholder="Username"
-							className="rounded bg-white w-32"
-						/>
-						<TouchableOpacity className="rounded bg-cyan-600">
-							<Text>Submit</Text>
-						</TouchableOpacity>
-					</View>
-					<View className="w-64 flex flex-row">
-						{/* create a textfield which loads in the current password */}
-						<TextInput
-							placeholder="Password"
-							className="rounded bg-white w-32"
-						/>
-						<TouchableOpacity className="rounded bg-cyan-600">
-							<Text>Submit</Text>
-						</TouchableOpacity>
-					</View>
-				</View>
+	  <View className="flex-1 items-center justify-center bg-white">
+		<SafeAreaView>
+		  {/* create a function which calls the setSettingsOpen function that closes the settings dialogue  */}
+		  <TouchableOpacity
+			onPress={() => {
+			  setSettingsOpen(false);
+			}}
+		  >
+			<Image source={require("../../assets/icons/back.png")} />
+		  </TouchableOpacity>
+		</SafeAreaView>
+		<Text className="mb-5 text-3xl font-bold">Settings</Text>
+		<View className="flex-1 items-center rounded w-4/5 py-5 bg-slate-200 my-5">
+		  <Text className="text-2xl p-2">Account</Text>
+		  <View className="grid">
+			<View className="w-64 flex flex-row">
+			  {/* create a textfield which loads in the current email */}
+			  <TextInput
+				placeholder="Email"
+				className="rounded bg-white w-32"
+			  />
+  
+			  <TouchableOpacity className="rounded bg-cyan-600">
+				<Text>Submit</Text>
+			  </TouchableOpacity>
 			</View>
-			<View className="flex-1 items-center rounded w-4/5 py-5 bg-slate-200 my-5">
-				<Text className="text-2xl p-2">Privacy</Text>
-				<View className="grid">
-					<View className="w-16">
-						<TouchableOpacity>
-							<Text>Tag</Text>
-						</TouchableOpacity>
-					</View>
-					<View className="w-16">
-						<TouchableOpacity>
-							<Text>Tag</Text>
-						</TouchableOpacity>
-					</View>
-					<View className="w-16">
-						<TouchableOpacity>
-							<Text>Tag</Text>
-						</TouchableOpacity>
-					</View>
-				</View>
+			<View className="w-64 flex flex-row">
+			  {/* create a textfield which loads in the current username */}
+			  <TextInput
+				placeholder="Username"
+				className="rounded bg-white w-32"
+			  />
+			  <TouchableOpacity className="rounded bg-cyan-600">
+				<Text>Submit</Text>
+			  </TouchableOpacity>
 			</View>
-			<View className="flex-1 items-center rounded w-4/5 py-5 bg-slate-200 my-5">
-				<View className="flex flex-row">
+			<View className="w-64 flex flex-row">
+			  {/* create a textfield which loads in the current password */}
+			  <TextInput
+				placeholder="Password"
+				className="rounded bg-white w-32"
+			  />
+			  <TouchableOpacity className="rounded bg-cyan-600">
+				<Text>Submit</Text>
+			  </TouchableOpacity>
+			</View>
+		  </View>
+		</View>
+		<View className="flex-1 items-center rounded w-4/5 py-5 bg-slate-200 my-5">
+		  <Text className="text-2xl p-2">Privacy</Text>
+		  <View className="grid">
+			<View className="w-16">
+			  <TouchableOpacity>
+				<Text>Tag</Text>
+			  </TouchableOpacity>
+			</View>
+			<View className="w-16">
+			  <TouchableOpacity>
+				<Text>Tag</Text>
+			  </TouchableOpacity>
+			</View>
+			<View className="w-16">
+			  <TouchableOpacity>
+				<Text>Tag</Text>
+			  </TouchableOpacity>
+			</View>
+		  </View>
+		</View>
+		<View className="flex-1 items-center rounded w-4/5 py-5 bg-slate-200 my-5">
+		  <View className="flex flex-row">
 					<Text className="text-2xl p-2">Notifications</Text>
 					{/* create a switch which toggles the notifications */}
 					<Switch disabled={false} />
@@ -168,6 +174,7 @@ export default function Likes() {
 				<Text className="text-4xl font-bold">Likes</Text>
 				{settingsButton}
 			</View>
+			
 			<ScrollView className="flex-1 rounded w-4/5 py-5 bg-slate-200 my-5">
 				<Text className="text-2xl p-2">CustomerID: Plate</Text>
 				<View className="grid">
