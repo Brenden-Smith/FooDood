6faf2376d9f84e76392d37d858ce674f99b4a7f5
stephenlyons18 <stephen.lyons18@gmail.com>
2023-02-23 22:28:11 -0800

    settings style changes

--- client/src/routes/home/Settings.tsx
+++ client/src/routes/home/Settings.tsx
@@ -21,37 +21,37 @@ export default function Settings(props: SettingsProps) {
 				</TouchableOpacity>
 			</SafeAreaView>
 			<Text className="mb-5 text-3xl font-bold">Settings</Text>
-			<View className="flex-1 items-center rounded w-4/5 py-5 bg-slate-200 my-5">
+			<View className="flex-1 items-center rounded w-4/5 py-5 bg-slate-500 my-5">
 				<Text className="text-2xl p-2">Account</Text>
-				<View className="grid">
-					<View className="w-64 flex flex-row">
+				<View className="flex flex-col">
+					<View className="w-64 flex flex-row my-2">
 						{/* create a textfield which loads in the current email */}
 						<TextInput
 							placeholder="Email"
 							className="rounded bg-white w-32"
 						/>
 
-						<TouchableOpacity className="rounded bg-cyan-600">
+						<TouchableOpacity className="rounded bg-cyan-600 m-4">
 							<Text>Submit</Text>
 						</TouchableOpacity>
 					</View>
-					<View className="w-64 flex flex-row">
+					<View className="w-64 flex flex-row my-2">
 						{/* create a textfield which loads in the current username */}
 						<TextInput
 							placeholder="Username"
 							className="rounded bg-white w-32"
 						/>
-						<TouchableOpacity className="rounded bg-cyan-600">
+						<TouchableOpacity className="rounded bg-cyan-600 m-4">
 							<Text>Submit</Text>
 						</TouchableOpacity>
 					</View>
-					<View className="w-64 flex flex-row">
+					<View className="w-64 flex flex-row my-2">
 						{/* create a textfield which loads in the current password */}
 						<TextInput
 							placeholder="Password"
 							className="rounded bg-white w-32"
 						/>
-						<TouchableOpacity className="rounded bg-cyan-600">
+						<TouchableOpacity className="rounded bg-cyan-600 m-4">
 							<Text>Submit</Text>
 						</TouchableOpacity>
 					</View>
