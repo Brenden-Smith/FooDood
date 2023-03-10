4b950428ce428bafde17a840004a68fcd28ce330
Brenden Smith <git@brenden-smith.com>
2023-03-09 18:13:58 -0800

    Global colors

---
+++ client/src/constants/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+	red: "#d90404",
+	blue: "#0063e6",
+	gray: "#777777",
+	white: "#ffffff",
+	black: "#000000",
+	green: "#aff7a8",
+};
--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,1 +1,2 @@
+export { colors } from "./colors";
 export { QueryKey } from "./QueryKey";
