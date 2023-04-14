5aa5c5ddbedd23d93040d29834b00cd66669d5fe
konrad <konradm.h.3@gmail.com>
2023-04-13 23:14:47 -0700

    update to PlateDescriptionModal.tsx

--- client/src/components/PlateDescriptionModal.tsx
+++ client/src/components/PlateDescriptionModal.tsx
@@ -1,81 +1,77 @@
 import { useCallback, useMemo } from "react";
-// modal
-import { Modal, StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import {
+  Modal,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 
-// button
-
-
-// icons
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-// colors
 import { colors } from "@/constants";
-import { Plate } from "@/types"
-type PlateDescriptionModalProps = {
-    visible: boolean;
-    onDismiss: () => void;
-    plateID: string;
-}
+import { Plate } from "@/types";
 import { usePlateData } from "@/hooks";
-
 import { styles } from "./PlatesDescriptionModalStyles";
 
-export function PlateDescriptionModal({ visible, onDismiss, plateID }: PlateDescriptionModalProps) {
-    // const plateData = usePlateData(plateID);
+type PlateDescriptionModalProps = {
+  visible: boolean;
+  onDismiss: () => void;
+  plateID: string;
+};
 
-    // similar to the above but listen for changes with the plateID
-    const plateData = usePlateData(plateID);
+export function PlateDescriptionModal({
+  visible,
+  onDismiss,
+  plateID,
+}: PlateDescriptionModalProps) {
+  const plateData = usePlateData(plateID);
 
-    return (
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={visible}
-            onRequestClose={onDismiss}
-        >
-            <View style={styles.centeredView}>
-                <View style={styles.modalView}>
-                    <View style={styles.modalHeader}>
-                        <Text style={styles.modalTitle}>{plateData.data?.data()?.name}</Text>
-                        <TouchableOpacity onPress={onDismiss}>
-                            <MaterialCommunityIcons
-                                name="close"
-                                size={24}
-                                color="black"
-                            />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.modalBody}>
-                        <Image
-                            source={{ uri: plateData.data?.data()!.image_url }}
-                            style={styles.modalImage}
-                        />
-
-                        <Text style={styles.subHeading}>Business Name</Text>
-                        <Text style={styles.modalText}>{plateData.data?.data()?.businessName}</Text>
-
-                        <Text style={styles.subHeading}>Description</Text>
-                        <Text style={styles.modalText}>{plateData.data?.data()?.description}</Text>
-
-                        <Text style={styles.subHeading}>Price</Text>
-                        <Text style={styles.modalText}>{plateData.data?.data()?.price}</Text>
-
-                        <View style={styles.modalTagsContainer}>
-                            <Text style={styles.subHeading}>Tags</Text>
-                            {/* map the tags into their own text component */}
-                            {plateData.data?.data()?.tags.map((tag: string) => (
-                                <Text 
-                                    style={styles.modalTag}
-                                    key={tag}
-                                >{tag}</Text>
-                            ))}
-                        </View>
-                    </View>
-                </View>
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onDismiss}
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}><View style={styles.imageContainer}>
+              <Image
+                source={{ uri: plateData.data?.data()?.image_url }}
+                style={styles.modalImage}
+              />
             </View>
-        </Modal>
-        
-    );
+          <View style={styles.modalHeader}>
+            <Text style={styles.modalTitle}>{plateData.data?.data()?.name}</Text>
+            <TouchableOpacity onPress={onDismiss}>
+              <MaterialCommunityIcons name="close" size={30} color="white" />
+            </TouchableOpacity>
+          </View>
+          
+          <View style={styles.modalBody}>
+            
 
+            <Text style={styles.subHeading}>Business Name</Text>
+            <Text style={styles.modalText}>{plateData.data?.data()?.businessName}</Text>
 
-    }
+            <Text style={styles.subHeading}>Description</Text>
+            <Text style={styles.modalText}>{plateData.data?.data()?.description}</Text>
+
+            <Text style={styles.subHeading}>Price</Text>
+            <Text style={styles.modalText}>{plateData.data?.data()?.price}</Text>
+
+            <View style={styles.modalTagsContainer}>
+              <Text style={styles.subHeading}>Tags</Text>
+              {plateData.data?.data()?.tags.map((tag: string) => (
+                <Text style={styles.modalTag} key={tag}>
+                  {tag}
+                </Text>
+              ))}
+            </View>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
--- client/src/components/PlatesDescriptionModalStyles.tsx
+++ client/src/components/PlatesDescriptionModalStyles.tsx
@@ -2,6 +2,8 @@ import { colors } from "@/constants";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+    
+      
     centeredView: {
         display: "flex",
         flex: 1,
@@ -12,6 +14,7 @@ export const styles = StyleSheet.create({
         display: "flex",
         flex: 0.8,
         width: "90%",
+        // to set opacity of background use : backgroundColor: "rgba(0,0,0,0.5)",
         backgroundColor: colors.white,
         borderRadius: 20,
         padding: 35,
@@ -34,6 +37,8 @@ export const styles = StyleSheet.create({
     modalTitle: {
         fontSize: 24,
         fontWeight: "bold",
+        color : colors.white,
+
     },
     modalBody: {
         display: "flex",
@@ -44,11 +49,14 @@ export const styles = StyleSheet.create({
     modalText: {
         fontSize: 16,
         textAlign: "center",
-        
+        color : colors.white,
+
+        // to allign text to bottom of the container
     },
     subHeading: {
         fontSize: 18,
         fontWeight: "bold",
+        color : colors.white,
     },
     modalTagsContainer: {
         display: "flex",
@@ -67,13 +75,24 @@ export const styles = StyleSheet.create({
         margin: 5,
     },
     modalImage: {
-        width: "80%",
-        height: "50%",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
         resizeMode: "cover",
         overflow: "hidden",
         borderRadius: 15,
         flex: 1,
         elevation: 6
+        // make sure its above the modal with elevation
     },
-
+imageContainer: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: "center",
+        alignItems: "center",
+      },
 });
