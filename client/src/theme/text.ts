78904e830fe2128f3262c38ad05ecbe6add80f6c
Brenden Smith <git@brenden-smith.com>
2023-05-05 01:25:07 -0700

    Font work

--- client/src/theme/text.ts
+++ client/src/theme/text.ts
@@ -3,34 +3,42 @@ import { StyleSheet } from "react-native";
 export const text = StyleSheet.create({
 	h1: {
 		fontSize: 32,
-		fontWeight: "bold",
+    fontWeight: "bold",
+    fontFamily: "Cabin",
   },
   h2: {
     fontSize: 24,
     fontWeight: "bold",
+    fontFamily: "Cabin",
   },
   h3: {
     fontSize: 20,
     fontWeight: "bold",
+    fontFamily: "Cabin",
   },
   h4: {
     fontSize: 16,
     fontWeight: "bold",
+    fontFamily: "Cabin",
   },
   h5: {
     fontSize: 14,
     fontWeight: "bold",
+    fontFamily: "Cabin",
   },
   h6: {
     fontSize: 12,
     fontWeight: "bold",
+    fontFamily: "Cabin",
   },
   p: {
     fontSize: 12,
     fontWeight: "normal",
+    fontFamily: "Cabin",
   },
   p2: {
     fontSize: 10,
     fontWeight: "normal",
+    fontFamily: "Cabin",
   },
 });
